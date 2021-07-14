27d1dbeb5261fc84a122f06bcd965706083a5ddd
dm1sh <me@dmitriy.icu>
2021-07-14 16:49:25 +0500

    Created bookshelf item component

---
+++ src/Bookshelf/BookItem/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import styles from "./BookItem.module.css";
+
+import { IBook } from "../../types/book";
+
+interface IBookItemProps extends IBook {}
+
+export const BookItem = ({ author, title, cover }: IBookItemProps) => {
+  return (
+    <div className={styles.bookCard}>
+      <div className={styles.cardHeading}>
+        <h1>{title}</h1>
+        <h2>{author}</h2>
+      </div>
+      <div className={styles.imageContainer}>
+        <img
+          className={styles.image}
+          src={cover}
+          alt={title + " cover picture"}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ src/types/book.ts
@@ -0,0 +1,7 @@
+export interface IBook {
+  title: string;
+  author: string;
+  cover?: string;
+  content: string;
+  hash?: string;
+}

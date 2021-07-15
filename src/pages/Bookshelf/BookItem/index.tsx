6044e7c37558ca056c72568b6b000934cc4f46f4
dm1sh <me@dmitriy.icu>
2021-07-16 04:39:12 +0500

    Fixed books without cover display and added links to book page

--- src/pages/Bookshelf/BookItem/index.tsx
+++ src/pages/Bookshelf/BookItem/index.tsx
@@ -3,23 +3,28 @@ import React from "react";
 import styles from "./BookItem.module.css";
 
 import { IBook } from "~/types/book";
+import { Link } from "wouter";
 
 interface IBookItemProps extends IBook {}
 
-export const BookItem = ({ author, title, cover }: IBookItemProps) => {
+export const BookItem = ({ author, title, cover, hash }: IBookItemProps) => {
   return (
-    <div className={styles.bookCard}>
-      <div className={styles.cardHeading}>
-        <h1>{title}</h1>
-        <h2>{author}</h2>
-      </div>
-      <div className={styles.imageContainer}>
-        <img
-          className={styles.image}
-          src={cover}
-          alt={title + " cover picture"}
-        />
-      </div>
-    </div>
+    <Link href={hash!}>
+      <a className={styles.bookCard}>
+        <div className={styles.cardHeading}>
+          <h1>{title}</h1>
+          <h2>{author}</h2>
+        </div>
+        {cover && (
+          <div className={styles.imageContainer}>
+            <img
+              className={styles.image}
+              src={cover}
+              alt={title + " cover picture"}
+            />
+          </div>
+        )}
+      </a>
+    </Link>
   );
 };
